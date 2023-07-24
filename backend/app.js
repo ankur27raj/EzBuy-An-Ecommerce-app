64c28deab62d55485b7684355df3fe8fd7b83fd6
ankur27raj <101596084+ankur27raj@users.noreply.github.com>
2023-07-24 13:06:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://ezbuy-frontend.onrender.com',
   credentials: true
 }));
 
